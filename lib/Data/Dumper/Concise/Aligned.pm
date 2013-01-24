74885b1b0db4e52c4dc4950f2ede4fdf7da241b4
Jeremy Mates <jmates@cpan.org>
2013-01-24 02:02:19 +0000

    Another test, resulting bugfixes, rename of exported Object to not clobber anything from Data/Dumper/Concise.pm.

--- lib/Data/Dumper/Concise/Aligned.pm
+++ lib/Data/Dumper/Concise/Aligned.pm
@@ -4,12 +4,15 @@ use 5.010000;
 use Scalar::Util qw/reftype/;
 use Text::Wrap qw/wrap/;
 
-our $VERSION = '0.20';
+our $VERSION = '0.21';
 
-BEGIN { @ISA = qw(Exporter) }
-@EXPORT = qw(DumperA DumperObject);
+require Exporter;
+require Data::Dumper;
 
-sub DumperObject {
+BEGIN { @ISA = qw/Exporter/ }
+@EXPORT = qw/DumperA DumperObjectA/;
+
+sub DumperObjectA {
   my $dd = Data::Dumper->new( [] );
   $dd->Terse(1)->Indent(0)->Useqq(1)->Deparse(1)->Quotekeys(0)->Sortkeys(1);
 }
@@ -20,7 +23,7 @@ sub DumperA {
   for my $o (@_) {
     if ( defined reftype $o) {
       $str_buf .=
-        wrap( $prefix, $prefix, DumperObject->Values( [$o] )->Dump ) . "\n";
+        wrap( $prefix, $prefix, DumperObjectA->Values( [$o] )->Dump ) . "\n";
     } else {
       $prefix = $o;
       $prefix .= ' ' unless $prefix =~ m/\s$/;
--- t/Data-Dumper-Concise-Aligned.t
+++ t/Data-Dumper-Concise-Aligned.t
@@ -3,7 +3,14 @@
 use strict;
 use warnings;
 
-use Test::More tests => 2;
+use Test::More tests => 3;
 BEGIN { use_ok('Data::Dumper::Concise::Aligned') }
 
-can_ok( 'Data::Dumper::Concise::Aligned', qw/DumperA DumperObject/ );
+can_ok( 'Data::Dumper::Concise::Aligned', qw/DumperA DumperObjectA/ );
+
+my @intervals = ( [ 2, 2, 1, 2, 2, 2, 1 ], [ 1, 2, 2, 2, 1, 2, 2 ] );
+is(
+  DumperA( M => \@intervals ),
+  "M [[2,2,1,2,2,2,1],[1,2,2,2,1,2,2]]\n",
+  'prefixed output'
+);
