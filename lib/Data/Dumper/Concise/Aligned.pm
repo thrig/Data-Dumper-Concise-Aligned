60745ee4a7202afbd2aaf4780e270b33c5f6b2c7
Jeremy Mates <jmates@cpan.org>
2015-02-07 21:40:46 +0000

    Version 0.25
     * No longer export DumperObjectA by default.
     * Majorly Maukeify Makefile.PL.
     * Add POD coverage tests. Document the undocumented method, whoops.
     * Make the POD tests mandatory. No quarter measures for this release, oh no.
     * Other documentation tweaks and improvements.

--- lib/Data/Dumper/Concise/Aligned.pm
+++ lib/Data/Dumper/Concise/Aligned.pm
@@ -6,14 +6,14 @@ use warnings;
 use Scalar::Util qw/reftype/;
 use Text::Wrap qw/wrap/;
 
-our $VERSION = '0.24';
+our $VERSION = '0.25';
 our @ISA;
 
 require Exporter;
 require Data::Dumper;
 
 BEGIN { @ISA = qw/Exporter/ }
-our @EXPORT = qw/DumperA DumperObjectA/;
+our @EXPORT = qw/DumperA/;
 
 sub DumperObjectA {
   my $dd = Data::Dumper->new( [] );
@@ -45,27 +45,57 @@ Data::Dumper::Concise::Aligned - even less indentation plus string prefix
 =head1 SYNOPSIS
 
   use Data::Dumper::Concise::Aligned;
-  warn DumperA This => \@something, That => \@otherthing;
+  warn DumperA S => \@input, D => \@output;
 
 =head1 DESCRIPTION
 
-Like L<Data::Dumper::Concise> except with even less indentation, and
-string prefixing of the wrapped-as-necessary output. Used in particular
-to look at data that needs to be shown in as compact a manner as
-possible for easy vertical comparison, for example:
+Like L<Data::Dumper::Concise> except with even less indentation, and string
+prefixing of the wrapped-as-necessary output. Used in particular to look at
+data that needs to be shown in as compact a manner as possible for easy
+vertical comparison (hypothetically, musical pitch numbers), for example:
 
   S [[2,2,1,2,2,2,1],[1,2,2,2,1,2,2]]
   D [[2,1,2,2,2,2,1],[2,2,1,2,2,1,2]]
 
-This could possibly be done via C<DumperF> of
-L<Data::Dumper::Concise>, but that's more typing, and not exactly the
-string prefix handling I wanted.
+This could possibly be done via C<DumperF> of L<Data::Dumper::Concise>, but
+that's more typing, and not exactly the string prefix handling I wanted.
 
-In C<vi> type editors, an C<ab> configuration along the lines of the
+=head2 Not Safe for Emacs Users
+
+In C<vi> type editors, an C<ab> editor configuration along the lines of the
 following can expand out to include the desired Dumper routine:
 
-  ab PUDD use Data::Dumper::Concise; warn Dumper
+  ab PUDD use Data::Dumper; warn Dumper
   ab PUCC use Data::Dumper::Concise::Aligned; warn DumperA
+
+=head1 FUNCTIONS
+
+=head2 DumperA
+
+Dumper, aligned, concise. Should be called with key value pairs, where the key
+is presumably some short label used to prefix the concisely dumped subsequent
+reference to some data structure. This function is exported by default.
+
+=head2 DumperObjectA 
+
+A mostly internal function used by B<DumperA>. Returns the L<Data::Dumper>
+object with formatting options as used by this module set. Not exported by
+default since version 0.25.
+
+=head1 SEE ALSO
+
+L<Data::Dump>, L<Data::Dumper::Concise>, L<Data::Dumper>, or so forth and so on
+from CPAN, one of which will hopefully meet your needs. If not, well, you can
+always write your own. Another good debugging tip is the C<%vd> C<printf>
+format, for seeing what the data really contains, but then you'll probably want
+to also know about L<ascii(7)>:
+
+  printf "%vd\n", "hi\r\tthere";
+
+L<Text::Wrap> is used to wrap text that strays beyond the usual punchcard
+inherited limits.
+
+L<ascii(7)>, L<vi(1)>
 
 =head1 AUTHOR
 
--- t/Data-Dumper-Concise-Aligned.t
+++ t/Data-Dumper-Concise-Aligned.t
@@ -3,7 +3,7 @@
 use strict;
 use warnings;
 
-use Test::More tests => 3;
+use Test::More tests => 4;
 BEGIN { use_ok('Data::Dumper::Concise::Aligned') }
 
 can_ok( 'Data::Dumper::Concise::Aligned', qw/DumperA DumperObjectA/ );
@@ -14,3 +14,6 @@ is(
   "M [[2,2,1,2,2,2,1],[1,2,2,2,1,2,2]]\n",
   'prefixed output'
 );
+
+isa_ok( Data::Dumper::Concise::Aligned::DumperObjectA,
+  'Data::Dumper', "object is object" );
